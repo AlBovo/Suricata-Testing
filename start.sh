eb2810e67c45eab82660b3de96d343e256071a04
Mark-74 <balduccimarco2007@gmail.com>
2025-05-20 16:54:20 +0200

    deleted useless line and added template ro custom.rules

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sysctl net.ipv4.conf.all.forwarding=
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
 iptables -I FORWARD -j NFQUEUE --queue-num 0 # pass to suricata only forwarded packets, not the ones from the host
